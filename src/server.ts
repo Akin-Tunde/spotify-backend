c9ea9c99be678d0b6adf723d56cab024221daf9e
Akin-Tunde <wang23hao@gmail.com>
2025-09-11 09:33:30 +0100

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,11 +18,13 @@ const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const redirect_uri = process.env.REDIRECT_URI;
 const frontend_uri = process.env.FRONTEND_URI;
 
-app.use(cors({
+/*app.use(cors({
     origin: frontend_uri,
     credentials: true
 }))
-.use(cookieParser());
+.use(cookieParser());*/
+
+app.use(cors({ origin: true, credentials: true })).use(cookieParser());
 
 // --- FIXED: Add body parser with a high limit for the image data ---
 app.use(express.json({ limit: '10mb' }));
